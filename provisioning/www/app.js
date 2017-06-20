53d74515c85e51ab90832651bd1f5477d1984f2e
Benjamin Tanz <btanz@gmx.de>
2017-06-21 00:20:05 +0200

    link and connect node app

--- provisioning/www/app.js
+++ provisioning/www/app.js
@@ -1,6 +1,10 @@
 'use strict';
 
 const express = require('express');
+const pg = require('pg');
+
+// Retrieve db connection string
+const dbConnectionString = 'postgresql://admin@' + process.env.POSTGRES_PORT_5432_TCP_ADDR + ':' + process.env.POSTGRES_PORT_5432_TCP_PORT + '/app';
 
 // Constants
 const PORT = 80;
@@ -13,7 +17,27 @@ app.set('view engine', 'pug')
 
 
 app.get('/', function (req, res) {
-  res.render('index', {title: 'App', greeting: 'Hello, world'});
+  const results = [];
+  // Get a Postgres client 
+  pg.connect(dbConnectionString, (err, client, done) => {
+    // Handle connection errors
+    if(err) {
+      done();
+      console.log(err);
+      return res.status(500).json({success: false, data: err});
+    }
+    // SQL Query 
+    const query = client.query('SELECT * FROM messages;');
+    // Stream results back
+    query.on('row', (row) => {
+      results.push(row);
+    });
+    // After data is returned, close connection and return results
+    query.on('end', () => {
+      done();
+      return res.render('index', {title: 'App', greeting: results[0].message});
+    });
+  });
 });
 
 app.listen(PORT);
